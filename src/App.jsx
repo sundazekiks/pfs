658fe516385cedd879eb68701b76e2676d38d8d5
sundazekiks <abutontravis1@gmail.com>
2025-10-15 17:52:56 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const [currentPicture, setCurrentPicture] = useState(null);
+
+  const pictureArray = [
+    "https://images.pexels.com/photos/4909162/pexels-photo-4909162.jpeg",
+    "https://images.pexels.com/photos/31746297/pexels-photo-31746297.jpeg",
+    "https://images.pexels.com/photos/3225517/pexels-photo-3225517.jpeg",
+    "https://images.pexels.com/photos/3225515/pexels-photo-3225515.jpeg",
+    "https://images.pexels.com/photos/34273316/pexels-photo-34273316.jpeg",
+  ];
+
+
+  return (<div className="w-full">
+    <div className="w-full h-screen bg-black flex justify-center items-center relative m-0 p-0 overflow-hidden">
+      <img src="https://images.pexels.com/photos/31746297/pexels-photo-31746297.jpeg" alt="hero image" className="absolute w-full h-full object-cover overflow-hidden m-0 p-0 block blur-xs"/>
+      <div className="flex flex-col z-10 text-white">
+        <h1 className="font-doris text-[89px]">Welcome to My Page</h1>
+        <p className="text-center font-doris text-4xl">Full Stack Developer</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+    <div className="w-full h-screen bg-amber-900 relative m-0 p-0 overflow-hidden flex justify-center items-center">
+      <img src={`${currentPicture}`} alt="" className="absolute w-full h-full object-cover overflow-hidden blur-xs animate-fadeIn"/>
+      // container for pictures
+      <div className="flex gap-5 absolute bottom-20">
+        {pictureArray.map((picture, index) => <img key={index} src={picture} alt={`picture-${index}`} className="w-32 h-20 object-cover hover:scale-110 transition-transform cursor-pointer" onMouseOver={() => setCurrentPicture(picture)}/>)}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  </div>
   )
 }
 
-export default App
+export default App;
